5f94a76561cb8c700ced27a9c479caf6e5e2769c
Юрий Уденеев <yudeneev@yandex.ru>
2020-04-02 08:01:52 +0400

    Создал класс упржнение

---
+++ TrainWindowsFormsApp/TrainWindowsFormsApp/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainWindowsFormsApp
+{
+    class Exercise
+    {
+        public string text;
+        public int repeat;
+        public bool status;           // Выполнены все повторения или нет
+        public string load;
+        public string remark;
+        public string specification;  // Какая группа мышц и спецификация упражнения
+    }
+}
