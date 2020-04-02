ce2d66416802d547ed05f14380d12391feb6eef0
Юрий Уденеев <yudeneev@yandex.ru>
2020-04-02 15:07:26 +0400

    Создание первого столбца форм

--- TrainWindowsFormsApp/TrainWindowsFormsApp/Exercise.cs
+++ TrainWindowsFormsApp/TrainWindowsFormsApp/Exercise.cs
@@ -7,16 +7,19 @@ using System.Threading.Tasks;
 namespace TrainWindowsFormsApp
 {
     class Exercise
-    {
-        public string text { get; }
-        public int repeat { get; }
+    {     public string text { get; }
+        public int repeat { get; set; }
         public bool status { get; set; }           // Выполнены все повторения или нет
         public string load;
         public string remark;
 
-        public Exercise Get()
+        public Exercise(string text, int repeat, bool status, string load, string remark)
         {
-
+            this.text = text;
+            this.repeat = repeat;
+            this.status = status;
+            this.load = load;
+            this.remark = remark;
         }
     }
 }
--- TrainWindowsFormsApp/TrainWindowsFormsApp/MainForm.cs
+++ TrainWindowsFormsApp/TrainWindowsFormsApp/MainForm.cs
@@ -12,7 +12,14 @@ namespace TrainWindowsFormsApp
 {
     public partial class MainForm : Form
     {
+        private readonly int cellSize = 60;
+        private readonly int indentBetween = 10;
+        private int indentUpEdge = 60;
+        public int mapSize = 9;
 
+        private Label[] labelsMap;
+
+        Exercise[] exercises = new Exercise[3];
 
         public MainForm()
         {
@@ -21,52 +28,51 @@ namespace TrainWindowsFormsApp
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            exercises[0] = new Exercise("Бег", 3, false, "Кроссовок, один", "Ногами");
+            exercises[1] = new Exercise("Жим", 3, false, "Жми - разрешаю", "Руками");
+            exercises[2] = new Exercise("Тяга", 3, false, "Тяни, ты всё равно меня не вытянишь", "Мыслями");
 
+            InitMap();
+        }
+
+        private void InitMap()
+        {
+            labelsMap = new Label[mapSize];
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                if (i % 3 == 0) indentUpEdge += 40;
+                var newLabel = CreateLabels(50, i, 600);
+                Controls.Add(newLabel);
+                labelsMap[i] = newLabel;                
+            }   
+        }
+
+        private Label CreateLabels(int indentLeftEdge, int indexRow, int width)
+        {
+            int x = indentLeftEdge;
+            int y = indentUpEdge + indexRow * (indentBetween + cellSize);
+
+            var label = new Label
+            {
+                BackColor = Color.Red,
+                Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204))),
+                Size = new Size(width, 60),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Location = new Point(x, y)
+            };
+
+            return label;
         }
 
         private void exerciseLabel1_MouseDown(object sender, MouseEventArgs e)
         {
-            MessageBox.Show();
+            MessageBox.Show(exercises[0].remark);
         }
 
-        private void exerciseLabel2_MouseDown(object sender, MouseEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show();
-        }
 
-        private void exerciseLabel3_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel4_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel5_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel6_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel7_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel8_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
-        }
-
-        private void exerciseLabel9_MouseDown(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show();
         }
     }
 }
