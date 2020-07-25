e262f9af4ad6a841a95eb47aa1b2767cd756b1ff
Юрий Уденеев <yudeneev@yandex.ru>
2020-07-25 05:40:56 +0400

    Сделал псевдо-рандом

--- TrainWindowsFormsApp/TrainWindowsFormsApp/ExercisesType.cs
+++ TrainWindowsFormsApp/TrainWindowsFormsApp/ExercisesType.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrainWindowsFormsApp
+﻿namespace TrainWindowsFormsApp
 {
     public enum ExercisesType
     {
-        Trapezius = 0,
-        DeltoidMid = 1,
-        DeltoidRear = 2,
-        Biceps = 3,
-        Triceps = 4,
-        Forearm = 5,
-        ChestBase = 6,
-        ChestIsol = 7,
-        LatissimusBase = 8,
-        LatissimusIsol = 9,
-        AbsDinamic = 10,
-        AbsStatic = 11,
-        ExtensorBack = 12,
-        Quadriceps = 13,
-        BicepsHip = 14
+        Trapezius,
+        DeltoidFront,
+        DeltoidMid,
+        DeltoidRear,
+        Biceps,
+        Triceps,
+        ChestBase,
+        ChestIsol,
+        LatissimusBase,
+        LatissimusIsol,
+        AbsDinamic,
+        AbsStatic,
+        ExtensorBack,
+        Quadriceps,
+        BicepsHip,
+        Calf
     }
 }
--- TrainWindowsFormsApp/TrainWindowsFormsApp/SaveNewExerciseForm.cs
+++ TrainWindowsFormsApp/TrainWindowsFormsApp/SaveNewExerciseForm.cs
@@ -1,12 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TrainWindowsFormsApp
@@ -24,7 +18,7 @@ namespace TrainWindowsFormsApp
 
         private void SaveNewExercise_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < 16; i++)
             {
                 exercisesTypeListBox.Items.Add((ExercisesType)i);
             }
